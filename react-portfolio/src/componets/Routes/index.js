87cf0b0797d69b9425be6a2ba37a405fd9c4d3b3
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-04-14 20:06:42 -0700

    update

--- react-portfolio/src/componets/Routes/index.js
+++ react-portfolio/src/componets/Routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Home';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-import SideBar from '../SideBar';
+import About from '../About';
+import Resume from '../Resume';
+import Skills from '../Skills';
+import Portfolio from '../Portfolio';
+import Contact from '../Contact'
 
 
 export default function Routes() {
@@ -12,12 +14,11 @@ export default function Routes() {
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route path='/home' component={Home} />
-                <Route path='/orders' component={Orders} />
-                <Route path='/shoes' component={Shoes} />
-                <Route path='/suppliers' component={Suppliers} />
-                <Route path='/vendors' component={Vendors} />
-                <Route path='/admin' component={Admin} />
-                <Route path='/reports' component={Reports} />
+                <Route path='/skills' component={Skills} />
+                <Route path='/resume' component={Resume} />
+                <Route path='/portfolio' component={Portfolio} />
+                <Route path='/contact' component={Contact} />
+                <Route path='/about' component={About} />
             </Switch>
         </div>
     );
